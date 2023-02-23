8ecb8e69488492b58b26b5f5d28be52f7f44af53
Natalia Castro <ncastro212@misena.edu.co>
2023-02-23 18:43:12 -0500

    API

--- scripts/map.js
+++ scripts/map.js
@@ -1,41 +1,38 @@
-require([
-    "esri/config",
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/widgets/Directions",
-    "esri/widgets/BasemapToggle",
-    "esri/widgets/BasemapGallery"
+require(["esri/Map", "esri/views/MapView", "esri/widgets/Directions", "esri/layers/RouteLayer"], function(
+  Map,
+  MapView,
+  Directions,
+  RouteLayer
+) {
 
-  ], function(esriConfig, Map, MapView, BasemapToggle, Directions, BasemapGallery) {
+  // An authorization string used to access the basemap, geocoding and routing services
+  const apiKey = "AAPKc0b8ba2631154c479397b7b54ef30a317KkDtiXgQNidqHbd_SP-sm7BdeGE5jQ-mGWnCAgLB7dFTkGEXcLLe6SZHFRpr14m";;
 
-    esriConfig.apiKey = "AAPKc0b8ba2631154c479397b7b54ef30a317KkDtiXgQNidqHbd_SP-sm7BdeGE5jQ-mGWnCAgLB7dFTkGEXcLLe6SZHFRpr14m";
+  // create a new RouteLayer, required for Directions widget
+  const routeLayer = new RouteLayer();
 
-    const map = new Map({
-      basemap: "arcgis-topographic"
-    });
-
-    const view = new MapView({
-      container: "viewDiv",
-      map: map,
-      center: [-74.11701,4.71049],
-      zoom: 15
-    });
-
-    const basemapToggle = new BasemapToggle({
-      view: view,
-      nextBasemap: "arcgis-imagery"
-   });
-
-    view.ui.add(basemapToggle,"bottom-left");
-
-
-
-
-    
-
-    view.ui.add(basemapGallery, "top-right"); // Add to the view
-
-
+  // new RouteLayer must be added to the map
+  const map = new Map({
+    basemap: "topo-vector",
+    layers: [routeLayer]
   });
 
-  
+  const view = new MapView({
+    zoom: 14,
+    center: [-118.24, 34.05],
+    container: "viewDiv",
+    map: map
+  });
+
+  // new RouteLayer must be added to Directions widget
+  let directionsWidget = new Directions({
+    layer: routeLayer,
+    apiKey,
+    view
+  });
+
+  // Add the Directions widget to the top right corner of the view
+  view.ui.add(directionsWidget, {
+    position: "top-right"
+  });
+});
